bd34d3f3fc92f54b48f852a0320c28f107364f1a
pedrolorandi <pedrokl@hotmail.com>
2023-05-28 20:56:52 -0400

    Finish Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
+import "./Header.css";
+
+import { FaGithub, FaLinkedinIn, FaCopy, FaFileDownload } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import portfolio from "../assets/Pedro Lorandi - Resume.pdf";
 
 const Header = () => {
-  return <div className="header flex"></div>;
+  return (
+    <div className="header flex">
+      <h1>Pedro Lorandi</h1>
+      <div className="buttons flex">
+        <a href="https://github.com/pedrolorandi" target="_blank">
+          <FaGithub size="1.25em" title="github.com/pedrolorandi" />
+        </a>
+        <a href="https://www.linkedin.com/in/pedrolorandi/" target="_blank">
+          <FaLinkedinIn size="1.25em" title="linkedin.com/in/pedrolorandi/" />
+        </a>
+        <div className="flex email">
+          <FaCopy size="1.25em" title="Copy email" />
+          <span>hello@pedrolorandi.com</span>
+        </div>
+        <a
+          className="button"
+          href={portfolio}
+          download="Pedro Lorandi - Full-Stack Web Developer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFileDownload size="1.25em" />
+          <span>Download Resume</span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
